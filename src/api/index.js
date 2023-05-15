238dba52d9b3b8166a0f849e3499f08e44d5f9ab
Nguyễn Anh Tuấn <anh.tuan@jvb-corp.com>
2023-05-15 10:22:01 +0700

    Update api url

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "https://memories-app-1j1c.onrender.com/";
+const url = "https://memories-app-1j1c.onrender.com/posts";
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
